886d016b92f92503c697ac49f0fee576ab92ea82
M-Mrich <106932429+M-Mrich@users.noreply.github.com>
2022-06-11 14:26:58 +0200

    uncaugh

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000
 const cors = require("cors");
 require('dotenv').config();
+process.on('uncaughtException', function (err) {
+  console.error(err);
+  console.log("Node NOT Exiting...");
+});
 app.use(cors({
     origin:"*",
     methods:["GET","POST","PUT","DELETE"]
